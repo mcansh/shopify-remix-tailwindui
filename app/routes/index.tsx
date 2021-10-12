0c7dd096211aa26aba4d16fe1dbec716672b3be9
Logan McAnsh <logan@mcan.sh>
2021-10-11 20:56:57 -0400

    chore: useLoaderData
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,8 +5,7 @@ import type {
   MetaFunction,
   RouteComponent,
 } from "remix";
-import { Link, json } from "remix";
-import { useRouteData } from "remix";
+import { Link, json, useLoaderData } from "remix";
 
 import { formatMoney } from "~/lib/format-money";
 import stylesUrl from "~/styles/index.css";
@@ -36,7 +35,7 @@ let loader: LoaderFunction = async () => {
     { products },
     {
       headers: {
-        "Cache-Control": ""
+        "Cache-Control": "",
       },
     }
   );
@@ -49,13 +48,13 @@ let headers: HeadersFunction = ({ loaderHeaders }) => {
 };
 
 const Index: RouteComponent = () => {
-  let data = useRouteData<RouteData>();
+  let data = useLoaderData<RouteData>();
 
   return (
     <main className="px-4 mt-24 sm:mt-32">
       <div className="mx-auto text-center max-w-7xl">
         <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-          Beautiful digital design assets
+          Beautiful digital design assets!
         </h1>
         <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           Take your digital products to another level with our collection of UI
